1cd73238186c668a44bb627a2c0039397635e140
walterEDR <dr13018@ues.edu.sv>
2023-07-12 21:55:33 -1200

    prueba

---
+++ prueba.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AWS Prueba</title>
+</head>
+<body>
+    <h1>Curso de Especialización 2023</h1>
+</body>
+</html>
