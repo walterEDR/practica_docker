54c5adf660130eff23504652bfc393b8a767ac89
walterEDR <dr13018@ues.edu.sv>
2023-07-12 22:36:30 -1200

    Testo prueba 2

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   </head>
   <body>
     <h1>Hello, world!</h1>
-    <p>texto prueba dasdasd</p>
+    <p>texto prueba </p>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
